ca54e1eabd7f1d4b0dc903e0ddc1b3466ce7eeb8
Adam Karlsten <adamkarlsten@gmail.com>
2021-06-12 14:52:26 +0200

    -

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,17 @@
+import * as THREE from 'three'
+
 export const PLANE_SIZE = 1000
 export const CUBE_AMOUNT = 100
+export const COLORS = {
+  red: {
+    hex: '#ff0000',
+    three: new THREE.Color(0xff0000)
+  },
+  yellow: {},
+  pink: {
+    hex: '#ff69b4',
+    three: new THREE.Color(0xff69b4)
+  },
+  green: {},
+  blue: {}
+}
