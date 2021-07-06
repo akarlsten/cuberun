c1a75742c31c983f35ee226d1888f87bc55e7f50
Adam Karlsten <adamkarlsten@gmail.com>
2021-07-06 15:54:02 +0200

    Adjusts loader

--- src/components/html/Overlay.js
+++ src/components/html/Overlay.js
@@ -4,8 +4,6 @@ import { useState, useEffect } from 'react'
 import Loader from './CustomLoader'
 import Author from './Author'
 
-import cubeRunLogo from '../../textures/cuberun-logo.png'
-
 import '../../styles/gameMenu.css'
 
 import { useStore } from '../../state/useStore'
@@ -13,6 +11,7 @@ import { useStore } from '../../state/useStore'
 const Overlay = () => {
   const [shown, setShown] = useState(true)
   const [opaque, setOpaque] = useState(true)
+  const [hasLoaded, setHasLoaded] = useState(false)
   const { active, progress } = useProgress()
 
   const gameStarted = useStore(s => s.gameStarted)
@@ -40,6 +39,12 @@ const Overlay = () => {
     localStorage.setItem('musicEnabled', JSON.stringify(musicEnabled))
   }, [musicEnabled])
 
+  useEffect(() => {
+    if (progress >= 100) {
+      setHasLoaded(true)
+    }
+  }, [progress])
+
   const handleStart = () => {
     setGameStarted(true)
   }
@@ -51,17 +56,19 @@ const Overlay = () => {
   return shown ? (
     <div onClick={() => setHasInteracted()} className={`game__container`} style={{ opacity: shown ? 1 : 0, background: opaque ? '#141622FF' : '#141622CC' }}>
       <div className="game__menu">
-        <img className="game__logo" src={cubeRunLogo} alt="Cuberun Logo" />
+        <img className="game__logo" src="cuberun-logo.png" alt="Cuberun Logo" />
         <div className="game__subcontainer">
-          {active ? (
+          {active && !hasLoaded ? (
             <Loader active={active} progress={progress} />
           ) : (
             <>
               <button onClick={handleStart} className="game__menu-button">{'STA>RT'}</button>
               <div className="game__menu-options">
                 <button onClick={handleMusic} className="game__menu-button game__menu-button-music">{`TURN MUSIC ${musicEnabled ? 'OF>F' : 'O<N'}`}</button>
-                <span className="game__menu-musicinfo">some visual effects depend on the music</span>
-                <span className="game__menu-controls">Controls: ⬅ a / d ➡ </span>
+                <span className="game__menu-controls">
+                  <p>Controls</p>
+                  ⬅ a / d ➡
+                </span>
                 <span className="game__menu-warning">Photosensitivity warning - Game contains flashing lights</span>
                 <Author />
               </div>
